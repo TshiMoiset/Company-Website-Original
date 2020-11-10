8e8b55d6ff41a0ed000dc0f0cafef900dfd8dc37
Moise Tshibwabwa <moise.tshibwabwa@learnet.se>
2020-11-11 00:42:02 +0100

    Save: New Buttons

--- style.js
+++ style.js
@@ -13,3 +13,54 @@ function dropModel() {
       modelText.style.display = "inline";
     }
   }
+
+  
+  function dropScreen() {
+    var end = document.getElementById("end");
+    var screenText = document.getElementById("screen");
+    var btnText = document.getElementById("infoScreen");
+
+    if (end.style.display === "none") {
+        end.style.display = "inline";
+      btnText.innerHTML = "Screen/Display";
+      screenText.style.display = "none";
+    } else {
+        end.style.display = "none";
+      btnText.innerHTML = "Screen/Display";
+      screenText.style.display = "inline";
+    }
+  }
+
+  
+  function dropFeatures() {
+    var end = document.getElementById("endfeatures");
+    var featuresText = document.getElementById("features");
+    var btnText = document.getElementById("infoFeatures");
+
+    if (end.style.display === "none") {
+        end.style.display = "inline";
+      btnText.innerHTML = "Features & Capacity";
+      featuresText.style.display = "none";
+    } else {
+        end.style.display = "none";
+      btnText.innerHTML = "Features & Capacity";
+      featuresText.style.display = "inline";
+    }
+  }
+
+  
+  function dropConnections() {
+    var end = document.getElementById("endconnections");
+    var connectionsText = document.getElementById("connections");
+    var btnText = document.getElementById("infoConnections");
+
+    if (end.style.display === "none") {
+        end.style.display = "inline";
+      btnText.innerHTML = "Connections";
+      connectionsText.style.display = "none";
+    } else {
+        end.style.display = "none";
+      btnText.innerHTML = "Connections";
+      connectionsText.style.display = "inline";
+    }
+  }
